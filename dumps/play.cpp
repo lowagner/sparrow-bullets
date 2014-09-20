3eb88e05a10cdfc19b26f92e16bc0f6a97e244be
lowagner <lowagner@gmail.com>
2014-09-20 19:27:17 +0200

    actual bug fix

--- dumps/play.cpp
+++ dumps/play.cpp
@@ -249,7 +249,7 @@ Play::reset()
             blocks[i].id = i;
             boxes[i].id = i;
         }
-        blocks[4].id = 1234;
+        boxes[4].id = 1234;
     }
     else if ( level == 8 )
     {
